1ee555d269053b30c8269a6755add98ddf08420a
Marcin Kwiatkowski <marcin@frodigo.com>
2025-10-05 13:45:17 +0200

    feat: added published post manager

--- Projects/Testtrack/M1 - Django Learning/mysite/blog/models.py
+++ Projects/Testtrack/M1 - Django Learning/mysite/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.conf import settings
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -22,6 +27,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-published_at',)
