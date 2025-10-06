b7ce0822296d1921a9f7f4d7bd7ee7e72aee0706
Marcin Kwiatkowski <marcin@frodigo.com>
2025-10-06 08:14:00 +0200

    refactor: refactor views to use classes

--- Projects/Testtrack/M1 - Django Learning/mysite/blog/views.py
+++ Projects/Testtrack/M1 - Django Learning/mysite/blog/views.py
@@ -1,33 +1,26 @@
-from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.get_page(page_number)
-
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        status=Post.Status.PUBLISHED,
-        slug=post,
-        published_at__year=year,
-        published_at__month=month,
-        published_at__day=day
-    )
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post/detail.html'
+    context_object_name = 'post'
 
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post}
-    )
+    def get_object(self, queryset=None):
+        return get_object_or_404(
+            Post,
+            status=Post.Status.PUBLISHED,
+            slug=self.kwargs['post'],
+            published_at__year=self.kwargs['year'],
+            published_at__month=self.kwargs['month'],
+            published_at__day=self.kwargs['day']
+        )
