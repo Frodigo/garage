3cdc4350645422d9aad32305ca045ed08e123a73
Marcin Kwiatkowski <marcin@frodigo.com>
2025-10-08 07:44:43 +0200

    feat: added basic search to blog

--- Projects/Testtrack/M1 - Django Learning/mysite/blog/urls.py
+++ Projects/Testtrack/M1 - Django Learning/mysite/blog/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
         views.post_comment,
         name='post_comment'
     ),
-    path('feed/', LatestPostsFeed(), name='post_feed')
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search')
 ]
